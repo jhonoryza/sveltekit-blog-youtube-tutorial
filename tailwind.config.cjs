4080e91caece07b805af0a79b3b2e792af8e8cfa
fajar sidik priatna <jardik.oryza@gmail.com>
2023-08-06 00:35:09 +0700

    add quick sand font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   plugins: [require('@tailwindcss/typography')],
   theme: {
     extend: {},
+    fontFamily: {
+      'quicksand': ['Quicksand', 'sans-serif']
+    },
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.5rem' }],
       sm: ['0.875rem', { lineHeight: '1.5rem' }],
